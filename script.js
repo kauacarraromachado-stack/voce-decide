401b47291122098fed103eaf401efe87f6a973f5
kauacarraromachado-stack <kaua.carraro.machado@escola.pr.gov.br>
2025-10-28 08:35:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 const avanca = document.querySelectorAll(".btn-proximo")
 
 avanca.forEach(button => 
-button.addEventListener('click')
+button.addEventListener('click'), function(){
+     const atual=document.querySelector('.ativo');
+     const proximopasso = 'passo-'+this.getAtribute(data-proximo);
+
+
+     atual.classList.remove('ativo;')
+    document.getElementById(proximopasso).classList.add('ativo');
+
+
+
+
+
+
+
+}
+
     )
